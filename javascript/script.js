777f8d3e6126b972534884ef5325c27d50427210
Magda <57455351+BMagda2911@users.noreply.github.com>
2021-11-10 14:16:21 +0100

    added clear log btn & enhanced script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,44 +1,39 @@
-function addListItem() {
+function addOrRemoveListItem() {
 		let inputValue = $("input").val();
 		if(inputValue !== '') {
 			let listElement = $("<li class='list-group-item list-group-item-action list-group-item-success align-middle'></li>").text(inputValue);
 			$(listElement).append("<button type='button' class='btn-close remove' aria-label='Close'></button>");
-			$("#mylist").append(listElement);
+			$("#myList").append(listElement);
 			$("input").val(""); // clears the input field
 			// removes parent of the current object, which is parent of the remove button, the <li> element.
 			$(".remove").on("click", function() {
-			$(this).parent().remove();
+				$(this).parent().remove();
 			});
 		}
 }
 
-function addLogStamp() {
-	let timeNDate = $("<li class='item align-middle'>Item added on " + document.lastModified + " </li>");
+function addOrRemoveLogStamp() {
+	let timeNDate = $("<li class='item align-middle remove-log'>Item added on " + document.lastModified + " </li>");
 	$("#logList").append(timeNDate);
+	$("#clearLog").on("click", function() {
+		$(this).next().remove();
+		location.reload();
+	});
 }
-
 
 $(function() {
 	$("#inputField").on("keyup", function(event) {
 		// number 13 is the "Enter" key on the keyboard
 		if (event.keyCode === 13) {
-			event.preventDefault(); // cancel the default action, if needed
-			//event handler
-			addListItem();
-			addLogStamp();
+			event.preventDefault(); // cancel the default action
+			// event handler
+			addOrRemoveListItem();
+			addOrRemoveLogStamp();
 		}
 	});
 	$("#add").on("click", function() {
-		//event handler
-		addListItem();
+		// event handler
+		addOrRemoveListItem();
+		addOrRemoveLogStamp();
 	});
 });
-
-
-
-$(function() {
-	$("#add").on("click", function(){
-		addLogStamp();
-	});
-});
-
